c5f34ece70cb9e84a0c0d573cb4f315ee9d7b09e
SHIVAM  SAINI <69237471+shivamsaini006@users.noreply.github.com>
2023-09-23 12:41:56 +0530

    Update MoviebookingappApplicationTests.java

--- src/test/java/com/moviebookingapp/MoviebookingappApplicationTests.java
+++ src/test/java/com/moviebookingapp/MoviebookingappApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class MoviebookingappApplicationTests {
 
