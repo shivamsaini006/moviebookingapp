0e975a3895025b19add2805b031de2524b55f8a1
SHIVAM  SAINI <69237471+shivamsaini006@users.noreply.github.com>
2023-09-23 12:34:51 +0530

    Update MoviesRepoTest.java

--- src/test/java/com/moviebookingapp/Repository/MoviesRepoTest.java
+++ src/test/java/com/moviebookingapp/Repository/MoviesRepoTest.java
@@ -14,6 +14,7 @@ import static org.assertj.core.api.Assertions.*;
 import com.moviebookingapp.models.CompositeKey;
 import com.moviebookingapp.models.Movies;
 
+@Disabled
 @SpringBootTest
 class MoviesRepoTest {
 
