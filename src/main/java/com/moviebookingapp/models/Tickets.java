834b290f5682c875176bb34315ac75fdda6c3e6c
SHIVAM  SAINI <69237471+shivamsaini006@users.noreply.github.com>
2023-09-23 11:44:40 +0530

    Update Tickets.java

--- src/main/java/com/moviebookingapp/models/Tickets.java
+++ src/main/java/com/moviebookingapp/models/Tickets.java
@@ -29,37 +29,4 @@ public class Tickets {
 	@NotNull(message="Enter your seat choice")
 	private List<String> seatNumber;
 
-
-	public Tickets(Movies movie, @NotNull(message = "Please enter number of tickets you want to book") int noOfTickets,
-			@NotNull(message = "Enter your seat choice") List<String> seatNumber) {
-		super();
-		this.movie = movie;
-		this.noOfTickets = noOfTickets;
-		this.seatNumber = seatNumber;
-	}
-
-	public Movies getMovie() {
-		return movie;
-	}
-
-	public void setMovie(Movies movie) {
-		this.movie = movie;
-	}
-
-	public int getNoOfTickets() {
-		return noOfTickets;
-	}
-
-	public void setNoOfTickets(int noOfTickets) {
-		this.noOfTickets = noOfTickets;
-	}
-
-	public List<String> getSeatNumber() {
-		return seatNumber;
-	}
-
-	public void setSeatNumber(List<String> seatNumber) {
-		this.seatNumber = seatNumber;
-	}
-
 }
