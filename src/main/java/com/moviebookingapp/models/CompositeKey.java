0d6e7b0398e80b6e6011362d9b0f7447170c5bdf
SHIVAM  SAINI <69237471+shivamsaini006@users.noreply.github.com>
2023-09-23 11:44:23 +0530

    Update CompositeKey.java

--- src/main/java/com/moviebookingapp/models/CompositeKey.java
+++ src/main/java/com/moviebookingapp/models/CompositeKey.java
@@ -15,24 +15,4 @@ public class CompositeKey {
 	private String movieName;
 	private String theatreName;
 	
-	
-	
-	public CompositeKey(String movieName, String theatreName) {
-		super();
-		this.movieName = movieName;
-		this.theatreName = theatreName;
-	}
-	
-	public String getMovieName() {
-		return movieName;
-	}
-	public void setMovieName(String movieName) {
-		this.movieName = movieName;
-	}
-	public String getTheatreName() {
-		return theatreName;
-	}
-	public void setTheatreName(String theatreName) {
-		this.theatreName = theatreName;
-	}
 }
