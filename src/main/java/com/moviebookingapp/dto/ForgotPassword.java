830be3a1d7546f8d2bfa620f143818698dd678ef
SHIVAM  SAINI <69237471+shivamsaini006@users.noreply.github.com>
2023-09-23 11:45:42 +0530

    Update ForgotPassword.java

--- src/main/java/com/moviebookingapp/dto/ForgotPassword.java
+++ src/main/java/com/moviebookingapp/dto/ForgotPassword.java
@@ -14,33 +14,4 @@ public class ForgotPassword {
 	private String password;
 	private String confirmPassword;
 	
-	
-	
-	public ForgotPassword(String password, String confirmPassword) {
-		super();
-		this.password = password;
-		this.confirmPassword = confirmPassword;
-	}
-
-	public ForgotPassword() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-
-
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getConfirmPassword() {
-		return confirmPassword;
-	}
-	public void setConfirmPassword(String confirmPassword) {
-		this.confirmPassword = confirmPassword;
-	}
-	
-	
 }
